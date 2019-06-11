f657dd9f1b08bc06bb6f1b04a867522635c3b6de
Elias Kuiter <info@elias-kuiter.de>
2019-06-11 23:01:48 +0200

    Small changes

---
+++ main/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define NUM_DEBOUNCED_PINS 6
+#define NUM_REED_SWITCH 4
+#define REED_SWITCH_PINS 25, 26, 32, 0
+
+#endif
--- main/lib/debounced_pin.h
+++ main/lib/debounced_pin.h
@@ -3,10 +3,8 @@
 
 #include <stdint.h>
 #include "driver/gpio.h"
-#include "sdkconfig.h"
 
-#define MAX_DEBOUNCED_PINS CONFIG_REED_SWITCH_NUMBER
-
-void debounced_pin_init(uint32_t pin_index, gpio_num_t pin, int debounce_ms, uint32_t stack_size, uint32_t priority, uint32_t coreID, void (*handler)(void* arg, int state), void* arg);
+void debounced_pin_init(uint32_t pin_index, gpio_num_t pin, int debounce_ms, uint32_t stack_size,
+    uint32_t priority, uint32_t coreID, void (*handler)(void* arg, int state), void* arg);
 
 #endif
