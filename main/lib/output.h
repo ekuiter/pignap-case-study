ef83453f05764fbd87a19192918017359adbb46c
Elias Kuiter <info@elias-kuiter.de>
2019-07-03 16:58:20 +0200

    Treatment phase 2 PWM

--- main/lib/output.h
+++ main/lib/output.h
@@ -2,6 +2,7 @@
 #define OUTPUT_H
 
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 
 #define PIN_BIT(x) (1ULL<<x)
 
@@ -10,5 +11,8 @@ void output_set(gpio_num_t pin, int level);
 void output_on(gpio_num_t pin);
 void output_off(gpio_num_t pin);
 void output_toggle(gpio_num_t pin);
+void output_init_pwm(ledc_timer_bit_t resolution, uint32_t frequency);
+void output_enable_pwm(gpio_num_t pin, ledc_channel_t channel, uint32_t duty);
+void output_disable_pwm(gpio_num_t pin, ledc_channel_t channel);
 
 #endif
