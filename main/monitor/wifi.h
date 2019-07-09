3ec527c13833184ec6bbaeb520df81fd5c7f6a93
Elias Kuiter <info@elias-kuiter.de>
2019-07-09 22:50:54 +0200

    Station Wifi settings

--- main/monitor/wifi.h
+++ main/monitor/wifi.h
@@ -1,11 +1,20 @@
 #ifndef WIFI_H
 #define WIFI_H
 
+#include "esp_wifi.h"
+
+typedef enum {
+    DISCONNECTED, CONNECTING, RETRYING, CONNECTED
+} wifi_sta_state_t;
+
 char* wifi_get_ap_ssid();
 char* wifi_get_ap_psk();
 char* wifi_get_sta_ssid();
 char* wifi_get_sta_psk();
+wifi_sta_state_t wifi_get_sta_state();
+ip4_addr_t wifi_get_sta_ip();
 void wifi_set_config(char* ap_ssid, char* ap_psk, char* sta_ssid, char* sta_psk);
+void wifi_reconnect();
 void _wifi_init();
 
 #endif
