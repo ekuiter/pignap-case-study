8a758f04f53f6f1f5c238e14d179e95f5e39bcb2
Elias Kuiter <info@elias-kuiter.de>
2019-07-12 11:13:50 +0200

    Total counter => today counter

--- main/control/controller.h
+++ main/control/controller.h
@@ -10,7 +10,7 @@ typedef enum {
 } controller_commodity_state_t;
 
 void controller_send(controller_event_t ev);
-int controller_get_total_counter();
+int controller_get_today_counter();
 int controller_get_filter_counter();
 int controller_get_isoflurane_counter();
 controller_commodity_state_t controller_get_filter_state();
