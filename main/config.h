3d64990c74abc392377596f8d9001201217d6065
Elias Kuiter <info@elias-kuiter.de>
2019-07-14 13:17:06 +0200

    Production flag

--- main/config.h
+++ main/config.h
@@ -4,7 +4,15 @@
 #include "lib/output.h"
 #include "lib/button.h"
 
+// comment this out to compile the code for my development board
+// #define IS_PRODUCTION
+
+extern const char* output_pin_names_development[], * output_pin_names_production[],
+    * button_pin_names_development[], * button_pin_names_production[]; // only used for logging
+extern const char* rtc_compile_date, * rtc_compile_time; // used for resetting the RTC
+
 // OUTPUTS
+#ifdef IS_PRODUCTION
 #define OUTPUT_GPIO_LED_1_GREEN       14
 #define OUTPUT_GPIO_LED_1_RED         12
 #define OUTPUT_GPIO_LED_2_GREEN       13
@@ -20,8 +28,25 @@
 #define OUTPUT_GPIO_MAGNETIC_VALVE_3  32
 #define OUTPUT_GPIO_MAGNETIC_VALVE_4  33
 #define OUTPUT_GPIO_COUNTER           18
-
-extern const char* output_pin_names[]; // only used for logging
+#define OUTPUT_PIN_NAMES              output_pin_names_production
+#else
+#define OUTPUT_GPIO_LED_1_GREEN       14
+#define OUTPUT_GPIO_LED_1_RED         12
+#define OUTPUT_GPIO_LED_2_GREEN       13
+#define OUTPUT_GPIO_LED_2_RED         15
+#define OUTPUT_GPIO_LED_3_GREEN       2
+#define OUTPUT_GPIO_LED_3_RED         4
+#define OUTPUT_GPIO_LED_4_GREEN       23
+#define OUTPUT_GPIO_LED_4_RED         19
+#define OUTPUT_GPIO_LED_FILTER_FULL   9
+#define OUTPUT_GPIO_LED_NO_ISOFLURANE 10
+#define OUTPUT_GPIO_MAGNETIC_VALVE_1  25
+#define OUTPUT_GPIO_MAGNETIC_VALVE_2  26
+#define OUTPUT_GPIO_MAGNETIC_VALVE_3  32
+#define OUTPUT_GPIO_MAGNETIC_VALVE_4  33
+#define OUTPUT_GPIO_COUNTER           18
+#define OUTPUT_PIN_NAMES              output_pin_names_development
+#endif
 
 // bit mask that configures output pins
 #define OUTPUT_PIN_SELECT \
@@ -36,12 +61,17 @@ extern const char* output_pin_names[]; // only used for logging
 
 // bit mask that determines which outputs have inverted logic
 // inverted means: HIGH = output off, LOW = output on
-// TODO: I use this to simulate some pins with a seven-segment display,
-// this must be changed for the final product!
+#ifdef IS_PRODUCTION
 #define OUTPUT_INVERTED_SELECT \
     (PIN_BIT(OUTPUT_GPIO_MAGNETIC_VALVE_1) | PIN_BIT(OUTPUT_GPIO_MAGNETIC_VALVE_2) \
     | PIN_BIT(OUTPUT_GPIO_MAGNETIC_VALVE_3) | PIN_BIT(OUTPUT_GPIO_MAGNETIC_VALVE_4) \
     | PIN_BIT(OUTPUT_GPIO_COUNTER))
+#else
+#define OUTPUT_INVERTED_SELECT \
+    (PIN_BIT(OUTPUT_GPIO_MAGNETIC_VALVE_1) | PIN_BIT(OUTPUT_GPIO_MAGNETIC_VALVE_2) \
+    | PIN_BIT(OUTPUT_GPIO_MAGNETIC_VALVE_3) | PIN_BIT(OUTPUT_GPIO_MAGNETIC_VALVE_4) \
+    | PIN_BIT(OUTPUT_GPIO_COUNTER))
+#endif
 
 // PWM channels for blinking LEDs
 #define OUTPUT_CHANNEL_LED_1_RED LEDC_CHANNEL_1
@@ -50,6 +80,7 @@ extern const char* output_pin_names[]; // only used for logging
 #define OUTPUT_CHANNEL_LED_4_RED LEDC_CHANNEL_4
 
 // BUTTONS
+#ifdef IS_PRODUCTION
 #define BUTTON_GPIO_REED_SWITCH_1    37
 #define BUTTON_GPIO_REED_SWITCH_2    38
 #define BUTTON_GPIO_REED_SWITCH_3    34
@@ -57,8 +88,17 @@ extern const char* output_pin_names[]; // only used for logging
 #define BUTTON_GPIO_RESET_FILTER     27
 #define BUTTON_GPIO_RESET_ISOFLURANE 0
 #define BUTTON_GPIO_VOLTAGE_MONITOR  5
-
-extern const char* button_pin_names[]; // only used for logging
+#define BUTTON_PIN_NAMES             button_pin_names_production
+#else
+#define BUTTON_GPIO_REED_SWITCH_1    37
+#define BUTTON_GPIO_REED_SWITCH_2    38
+#define BUTTON_GPIO_REED_SWITCH_3    34
+#define BUTTON_GPIO_REED_SWITCH_4    35
+#define BUTTON_GPIO_RESET_FILTER     27
+#define BUTTON_GPIO_RESET_ISOFLURANE 0
+#define BUTTON_GPIO_VOLTAGE_MONITOR  5
+#define BUTTON_PIN_NAMES             button_pin_names_production
+#endif
 
 // bit mask that configures buttons as input pins
 #define BUTTON_PIN_SELECT \
@@ -69,31 +109,46 @@ extern const char* button_pin_names[]; // only used for logging
 
 // bit mask that determines which buttons have inverted logic
 // inverted means: HIGH = button up, LOW = button down
-// TODO: on my development board, only GPIO 0 is inverted, this
-// must be changed for the final product!
 // the voltage monitor is considered a button too, it is "pressed down"
 // when the voltage drops and the device must save its data
+#ifdef IS_PRODUCTION
 #define BUTTON_INVERTED_SELECT \
     (PIN_BIT(BUTTON_GPIO_RESET_ISOFLURANE) | PIN_BIT(BUTTON_GPIO_VOLTAGE_MONITOR))
+#else
+#define BUTTON_INVERTED_SELECT \
+    (PIN_BIT(BUTTON_GPIO_RESET_ISOFLURANE) | PIN_BIT(BUTTON_GPIO_VOLTAGE_MONITOR))
+#endif
 
 // TREATMENT PROCESS
 #define TREATMENT_PROCESS_NUM          4
-#define TREATMENT_PHASE_1_DURATION     2 // TODO: 75
-#define TREATMENT_PHASE_2_DURATION     2 // TODO: 15
 #define TREATMENT_TOTAL_DURATION       (TREATMENT_PHASE_1_DURATION + TREATMENT_PHASE_2_DURATION)
 #define TREATMENT_LED_BLINK_RESOLUTION LEDC_TIMER_10_BIT
 #define TREATMENT_LED_BLINK_FREQUENCY  1
 #define TREATMENT_LED_BLINK_DUTY       512
+#ifdef IS_PRODUCTION
+#define TREATMENT_PHASE_1_DURATION     75
+#define TREATMENT_PHASE_2_DURATION     15
+#else
+#define TREATMENT_PHASE_1_DURATION     2
+#define TREATMENT_PHASE_2_DURATION     2
+#endif
 
 // CONTROLLER
 #define CONTROLLER_COUNTER_ON_DURATION  1000
 #define CONTROLLER_COUNTER_OFF_DURATION 500
-#define CONTROLLER_FILTER_WARN          8 // TODO
-#define CONTROLLER_ISOFLURANE_WARN      10 // TODO
-#define CONTROLLER_FILTER_TOLERANCE     2 // TODO
-#define CONTROLLER_ISOFLURANE_TOLERANCE 2 // TODO
 #define CONTROLLER_FILTER_MAX           (CONTROLLER_FILTER_WARN + CONTROLLER_FILTER_TOLERANCE)
 #define CONTROLLER_ISOFLURANE_MAX       (CONTROLLER_ISOFLURANE_WARN + CONTROLLER_ISOFLURANE_TOLERANCE)
+#ifdef IS_PRODUCTION
+#define CONTROLLER_FILTER_WARN          8
+#define CONTROLLER_ISOFLURANE_WARN      10
+#define CONTROLLER_FILTER_TOLERANCE     2
+#define CONTROLLER_ISOFLURANE_TOLERANCE 2
+#else
+#define CONTROLLER_FILTER_WARN          250
+#define CONTROLLER_ISOFLURANE_WARN      250
+#define CONTROLLER_FILTER_TOLERANCE     30
+#define CONTROLLER_ISOFLURANE_TOLERANCE 30
+#endif
 
 // I2C
 #define I2C_SDA_PIN   21
@@ -102,7 +157,9 @@ extern const char* button_pin_names[]; // only used for logging
 
 // LCD
 // comment the next line out to remove logging to LCD display
+#ifndef IS_PRODUCTION
 #define LCD_ENABLED
+#endif
 #define LCD_I2C_ADDRESS 0x27
 #define LCD_NUM_COLS    16
 #define LCD_NUM_ROWS    2
@@ -131,13 +188,11 @@ extern const char* button_pin_names[]; // only used for logging
 // Have a look at the administration page (http://192.168.4.1/admin) to see if
 // RTC and history memory are correct.
 
-extern const char* rtc_compile_date, * rtc_compile_time; // used for resetting the RTC
-
 // WIFI
 #define WIFI_SETTING_BUF_LEN    48
 
 // WIFI ACCESS POINT
-#define WIFI_AP_SSID            "PIGNAP"
+#define WIFI_AP_SSID            "Pignap"
 #define WIFI_AP_DEFAULT_PSK     ***REMOVED***
 #define WIFI_AP_MAX_CONNECTIONS 5
 
