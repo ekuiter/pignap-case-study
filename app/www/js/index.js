2e460259db85cd933cc62c41f275324935701ce1
Elias Kuiter <info@elias-kuiter.de>
2019-08-03 00:56:27 +0200

    Basic app added

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,46 +1,60 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var ipAddress = '192.168.4.1';
+var wifiSSID = 'Pignap';
+var wifiAlgorithm = 'WPA';
+var wifiPSK = 'hcp-technology';
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+function setStatusFn(status) {
+    return function() {
+        document.getElementById('status').innerHTML = status;
+    };
+}
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function setStatus(status) {
+    return setStatusFn(status)();
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+function tryToOpen() {
+    function tryAgain() {
+        new Ping().ping(
+            [{query: ipAddress, timeout: 1, retry: 1, version: 'v4'}],
+            function(res) {
+                if (res[0].response.status === 'success')
+                    cordova.InAppBrowser.open('http://' + ipAddress, '_self');
+                else
+                    tryAgain();
+            }, tryAgain);
     }
+    tryAgain();
+}
+
+window.tryToConnect = function tryToConnect() {
+    setStatus('<img src="img/loading.svg" width="40" height="40">');
+    WifiWizard2.scan().then(function(networks) {
+        var pignapSSIDs = [];
+        for (var i = 0; i < networks.length; i++)
+            if (networks[i].SSID.indexOf(wifiSSID) !== -1)
+            pignapSSIDs.push(networks[i].SSID);
+        if (pignapSSIDs.length === 0)
+            setStatus('Es ist kein Pignap-WLAN in Reichweite.' +
+                '<br><a class="button" href="#" onclick="deviceready();">Erneut versuchen</a>');
+        else {
+            WifiWizard2.connect(pignapSSIDs[0], false, wifiPSK, wifiAlgorithm, false).then(
+                tryToOpen,
+                setStatusFn('Die Verbindung zum Pignap-WLAN konnte nicht hergestellt werden.' +
+                    '<br><a class="button" href="#" onclick="deviceready();">Erneut versuchen</a>'));
+        }
+    }, setStatusFn('Verbinden Sie sich manuell mit dem Pignap-WLAN, um fortzufahren.' +
+        '<br><a class="button" href="#" onclick="deviceready();">Erneut versuchen</a>'));
+}
+
+window.deviceready = function deviceready() {
+    setStatus('<img src="img/loading.svg" width="40" height="40">');
+    WifiWizard2.getConnectedSSID().then(function(ssid) {
+        if (ssid.indexOf(wifiSSID) !== -1)
+            tryToOpen();
+        else
+            tryToConnect();
+    }, tryToConnect);
 };
 
-app.initialize();
+document.addEventListener('deviceready', deviceready);
