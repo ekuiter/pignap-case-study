86effa2275dfb93c78c4d35b66523b67f81e5876
Elias Kuiter <info@elias-kuiter.de>
2019-10-27 15:49:10 +0100

    Added third treatment phase

--- esp32/main/control/treatment_process.h
+++ esp32/main/control/treatment_process.h
@@ -6,7 +6,7 @@ typedef enum {
 } treatment_process_event_t;
 
 typedef enum {
-    IDLE, PHASE_1, PHASE_2, DONE, BLOCKED
+    IDLE, PHASE_1, PHASE_2, PHASE_3, DONE, BLOCKED
 } treatment_process_state_t;
 
 void treatment_process_send(int idx, treatment_process_event_t ev);
