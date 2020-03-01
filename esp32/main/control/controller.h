5fbc2d86b410098a796f40f501d639c55da68d86
Elias Kuiter <info@elias-kuiter.de>
2020-03-01 22:27:40 +0100

    Can reset filter at any time

--- esp32/main/control/controller.h
+++ esp32/main/control/controller.h
@@ -12,7 +12,6 @@ typedef enum {
 void controller_send(controller_event_t ev);
 controller_commodity_state_t controller_get_filter_state();
 controller_commodity_state_t controller_get_prefilter_state();
-bool controller_can_reset();
 bool controller_is_ready();
 void controller_init();
 
